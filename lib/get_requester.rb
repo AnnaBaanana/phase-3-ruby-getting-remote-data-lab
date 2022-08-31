aa99d66f9e0e4589f47c3c72d3938762754b5893
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-08-30 23:59:58 -0400

    commit tree 9a4bee5de0f1267923c5ad299e707fdafee7b792

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,29 @@
+require 'open-uri'
+require 'net/http'
+require 'json'
+require 'pry'
+
 # Write your code here
+class GetRequester
+    attr_accessor :url
+
+    def initialize(url)
+        @url = url
+        puts  "This #{url} was supplied"
+    end
+
+    def get_response_body
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+        #data = JSON.parse(response.body)
+    end
+
+    def parse_json
+        data = JSON.parse(self.get_response_body)
+    end
+
+end
+
+binding.pry
+
